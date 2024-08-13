13794f3d8a725d6325c65a3cad5b4cdc89403668
M.A.H.D. NAVARATHNE <140165356+Hasitha12389@users.noreply.github.com>
2024-08-13 11:13:53 +0530

    q1.scala

--- q1.scala
+++ q1.scala
@@ -3,7 +3,7 @@ object CaesarCiperaz{
 def encrypt(message: String,shift: Int): String = {
     message.map { x =>
         if(x.isLetter){
-            val base = if (x.isLower) 'a' else 'A'   // A = 65, a = 65 
+            val base = if (x.isLower) 'a' else 'A'   // A = 65, a = 97 // D = 68
             ((x - base + shift) % 26 + base).toChar 
         } else {
             x
@@ -11,8 +11,15 @@ def encrypt(message: String,shift: Int): String = {
     }
 }
 
-def decrypt(message: String,shift: Int): String = {
-    encrypt(message,shift)
+def decrypt(text: String,shift: Int): String = {
+    text.map { a =>
+        if(a.isLetter){
+            val base = if (a.isLower) 'a' else 'A'
+            ((a - base + (26 - shift)) % 26 + base).toChar        // 23 D = 68
+        } else {
+            a
+        }
+    }
 }
 
 def cipher(message: String,shift: Int, operation: (String,Int) => String): String = {
@@ -21,14 +28,14 @@ def cipher(message: String,shift: Int, operation: (String,Int) => String): Strin
  
 def main(args:Array[String]): Unit={
     val message = "ABCDE"
-    val shift = 5
+    val shift = 3
 
     println("\n---------Ceaser Cipher----------\n")
 
     val encrypted = cipher(message, shift,encrypt)
     println(s"Encrypterd message : $encrypted")
 
-    val decrypted = cipher(encrypted, 26-shift,decrypt) // to pass 26 - shift to decrypt
+    val decrypted = cipher(encrypted, shift,decrypt) 
     println(s"Decrypted message : $decrypted\n")
 
 }
